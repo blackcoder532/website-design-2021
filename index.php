93bdd986d6795a565c3dc287c3c8036531cc8228
blackcodersec <72689232+blackcodersec@users.noreply.github.com>
2021-10-08 03:21:41 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
-<?php
-
-// Comment these lines to hide errors
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require 'includes/config.php';
-require 'includes/functions.php';
-
-init();
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Hello</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1>HELLO -- WORLD</h1>>
+    
+</body>
+</html>>
